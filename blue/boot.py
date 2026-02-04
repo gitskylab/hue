b7f45d4b81a47ae86847118a9e9ab26cd8085526
gitskylab <dr.bips@proton.me>
2026-02-04 22:40:40 +0530

    Update boot.py

--- blue/boot.py
+++ blue/boot.py
@@ -1,1 +1,20 @@
+import ota
+import machine
 
+ota = ota.ota(
+  user="gitskylab",
+  repo="hue",
+  branch="main",
+  working_dir="blue",
+  files = ["boot.py", "main.py"]
+)
+
+try:
+    # Code that might raise an exception
+    ota.wificonnect()
+    if ota.update():
+        print("update complete.. rebooting...")
+        machine.reset()
+except OSError as e:
+    # Code to handle the error
+    print(f"error encountered: {e}")
